f52ae7ec057ff49fe7ca457e97a8b70defffd911
Jack Osei <jackoseik@gmail.com>
2025-01-04 00:25:37 +0400

    Initial commit: Vite setup with TypeScript, Material UI, and Tailwind CSS

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Button } from "@mui/material";
+
+const App: React.FC = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <Button variant="contained" color="primary">
+        ProfitPulse is Ready with Vite!
+      </Button>
+    </div>
+  );
+};
+
+export default App;
